7023d1f86809fce696477074ee346c8bd85e39c1
Sheron <sheron_carneiro@estudante.sesisenai.org.br>
2024-03-13 08:23:01 -0300

    Inicialização das classes

---
+++ class.js
@@ -0,0 +1,66 @@
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+    }
+}
+
+class Ship extends Obj{
+dir = 0
+pts = 0
+vida = 5
+
+draw_ship(){
+
+}
+
+atual_ship(){
+
+}
+point(){
+
+}
+colid(){
+
+}
+
+
+enemy(){
+
+}
+
+atual_enemy(){
+
+}
+
+redo(){
+
+}
+
+shot(){
+
+}
+
+atual_shot(){
+
+}
+
+redo(){
+
+}
+
+draw_space(){
+
+}
+
+redo(){
+
+}
+
+}
+ class Text{
+    
+ } 
